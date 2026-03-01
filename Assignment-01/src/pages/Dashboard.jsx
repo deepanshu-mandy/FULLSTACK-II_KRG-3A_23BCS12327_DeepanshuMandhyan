eb011aebe29373f50e5cdd6d5773676a8b1ee0de
Deepanshu Mandhyan <workofficial.deepanshu@gmail.com>
2026-03-01 22:30:59 +0530

    Added Assignment-01 project

---
+++ Assignment-01/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div class="p-10">
+      <h1 class="text-3xl font-bold mb-2">Dashboard</h1>
+      <p class="text-gray-600">Welcome to EcoTrack Dashboard</p>
+    </div>
+  )
+}
